6ca7214a62692870715b9b53a42dc85a7176d27f
andrey <naumov.a.n@icloud.com>
2023-05-01 12:15:54 +0700

    чтение из файла

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Note:
+    def __init__(self, id, title, body, created_at, updated_at):
+        self.id = id
+        self.title = title
+        self.body = body
+        self.created_at = created_at
+        self.updated_at = updated_at
+class NotesApp:
+    def __init__(self):
+        self.notes = []
+        self.load_notes()
 
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def load_notes(self):
+        try:
+            with open('notes.json', 'r') as f:
+                notes_data = json.load(f)
+                for note_data in notes_data:
+                    note = Note(note_data['id'], note_data['title'], note_data['body'], note_data['created_at'],
+                                note_data['updated_at'])
+                    self.notes.append(note)
+        except:
+            pass
